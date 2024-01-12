2bd309b57342bc1358e3df5bee3b08f6fab8e59e
Dhruv Garg <dhruv@Dhruvs-MacBook-Air.local>
2024-01-13 01:41:58 +0530

    models completed

---
+++ src/models/like.model.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const likeSchema = new mongoose.Schema({
+    video:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Video"
+    },
+    tweet:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'Tweet'
+    },
+    comment:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'Comment'
+    },
+    likedBy:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'User'
+    },
+}, {timestamps:true});
+
+export const Like = mongoose.model("Like", likeSchema);
